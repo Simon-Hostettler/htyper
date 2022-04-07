34065cdef05a4ee6e96669270073e99c35a0e3fd
Simon Hostettler <simon.hostettler@protonmail.com>
2022-04-07 10:12:05 +0200

    implemented randomization of words, relocated State Logic to TypingTest.hs

--- app/TypingTest.hs
+++ app/TypingTest.hs
@@ -1,1 +1,89 @@
 module TypingTest where
+
+import Brick.Main
+import Brick.Types
+import Brick.Widgets.Edit (handleEditorEvent)
+import Cursor.Simple.List.NonEmpty
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
+import Data.List.Split
+import System.Exit
+import System.Random (newStdGen)
+import System.Random.Shuffle (shuffle')
+
+newtype TestState = TestState
+  { text :: NonEmptyCursor TestWord
+  }
+  deriving (Show, Eq)
+
+data TestWord = TestWord
+  { word :: String,
+    input :: String
+  }
+  deriving (Show, Eq)
+
+buildInitialState :: Int -> IO TestState
+buildInitialState num_words = do
+  file <- readFile "lang/1000us.txt"
+  rng <- newStdGen
+  let word_list = words file
+  let sampled_words = take num_words (shuffle' word_list (length word_list) rng)
+  let test_words = map (\s -> TestWord {word = s, input = ""}) sampled_words
+  case NE.nonEmpty test_words of
+    Nothing -> die "No words in file"
+    Just ne -> pure TestState {text = makeNonEmptyCursor ne}
+
+cursorPosition :: NonEmptyCursor a -> Int
+cursorPosition cur = length (nonEmptyCursorPrev cur)
+
+getActiveCharLoc :: Int -> NonEmptyCursor TestWord -> Int
+getActiveCharLoc linelen cur = do
+  let activeLineNum = cursorPosition cur `div` linelen
+  let activeLine = chunksOf linelen (NE.toList (rebuildNonEmptyCursor cur)) !! activeLineNum
+  let posInLine = sum (map ((+ 1) . length . word) (take (cursorPosition cur `mod` linelen) activeLine))
+  posInLine + length (input (nonEmptyCursorCurrent cur))
+
+getActiveLines :: Int -> Int -> NonEmptyCursor TestWord -> [[TestWord]]
+getActiveLines numlines linelen cur = do
+  let activeLineNum = cursorPosition cur `div` linelen
+  let lines = chunksOf linelen (NE.toList (rebuildNonEmptyCursor cur))
+  take numlines (drop activeLineNum lines)
+
+handleTextInput :: TestState -> Char -> EventM n (Next TestState)
+handleTextInput s c =
+  case c of
+    ' ' -> do
+      let cur = text s
+      case nonEmptyCursorSelectNext cur of
+        Nothing -> continue s
+        Just cur' -> continue $ s {text = cur'}
+    _ -> do
+      let cur = text s
+      let curword = nonEmptyCursorCurrent cur
+      let updatedWord = TestWord {word = word curword, input = input curword ++ [c]}
+      let new_text = reverse (nonEmptyCursorPrev cur) ++ [updatedWord] ++ nonEmptyCursorNext cur
+      case NE.nonEmpty new_text of
+        Nothing -> continue s
+        Just ne -> do
+          case makeNonEmptyCursorWithSelection (cursorPosition cur) ne of
+            Nothing -> continue s
+            Just ne' -> continue $ s {text = ne'}
+
+handleBackSpaceInput :: TestState -> EventM n (Next TestState)
+handleBackSpaceInput s = do
+  let cur = text s
+  let curword = nonEmptyCursorCurrent cur
+  case input curword of
+    "" -> do
+      case nonEmptyCursorSelectPrev cur of
+        Nothing -> continue s
+        Just cur' -> continue $ s {text = cur'}
+    _ -> do
+      let updatedWord = TestWord {word = word curword, input = init (input curword)}
+      let new_text = reverse (nonEmptyCursorPrev cur) ++ [updatedWord] ++ nonEmptyCursorNext cur
+      case NE.nonEmpty new_text of
+        Nothing -> continue s
+        Just ne -> do
+          case makeNonEmptyCursorWithSelection (cursorPosition cur) ne of
+            Nothing -> continue s
+            Just ne' -> continue $ s {text = ne'}
--- app/UI.hs
+++ app/UI.hs
@@ -9,31 +9,19 @@ import Brick.Widgets.Center
 import Brick.Widgets.Core
 import Brick.Widgets.Edit (handleEditorEvent)
 import Cursor.Simple.List.NonEmpty
-import Data.List
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
-import Data.List.Split
 import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
-import System.Directory
-import System.Exit
+import System.Random (newStdGen)
+import System.Random.Shuffle (shuffle')
+import TypingTest
 
 ui :: IO ()
 ui = do
-  initialState <- buildInitialState
+  initialState <- buildInitialState 50
   endState <- defaultMain htyper initialState
   print endState
-
-newtype TestState = TestState
-  { text :: NonEmptyCursor TestWord
-  }
-  deriving (Show, Eq)
-
-data TestWord = TestWord
-  { word :: String,
-    input :: String
-  }
-  deriving (Show, Eq)
 
 data Input
   = Input
@@ -57,17 +45,8 @@ htyper =
       appChooseCursor = showFirstCursor,
       appHandleEvent = handleInputEvent,
       appStartEvent = pure,
-      appAttrMap = const $ attrMap mempty [(sel, fg red), (cor, fg green), (wrong, fg red), (norm, fg white)]
+      appAttrMap = const $ attrMap mempty [(sel, fg red), (cor, fg white), (wrong, fg red), (norm, fg brightBlack)]
     }
-
-buildInitialState :: IO TestState
-buildInitialState = do
-  file <- readFile "lang/1000us.txt"
-  let in_words = lines file
-  let test_words = map (\s -> TestWord {word = s, input = ""}) in_words
-  case NE.nonEmpty test_words of
-    Nothing -> die "No words in file"
-    Just ne -> pure TestState {text = makeNonEmptyCursor ne}
 
 drawUI :: TestState -> [Widget Name]
 drawUI ts =
@@ -80,23 +59,10 @@ drawUI ts =
                   map (hBox . map drawWord) (getActiveLines 3 15 cur)
       ]
 
-getActiveCharLoc :: Int -> NonEmptyCursor TestWord -> Int
-getActiveCharLoc linelen cur = do
-  let activeLineNum = cursorPosition cur `div` linelen
-  let activeLine = chunksOf linelen (NE.toList (rebuildNonEmptyCursor cur)) !! max 0 activeLineNum
-  let posInLine = sum (map ((+ 1) . length . word) (take (cursorPosition cur `mod` linelen) activeLine))
-  posInLine + length (input (nonEmptyCursorCurrent cur))
-
-getActiveLines :: Int -> Int -> NonEmptyCursor TestWord -> [[TestWord]]
-getActiveLines numlines linelen cur = do
-  let activeLineNum = cursorPosition cur `div` linelen
-  let lines = chunksOf linelen (NE.toList (rebuildNonEmptyCursor cur))
-  take numlines (drop activeLineNum lines)
-
 drawWord :: TestWord -> Widget n
 drawWord w =
   case input w of
-    "" -> str (word w ++ " ")
+    "" -> withAttr norm (str (word w ++ " "))
     _ -> hBox $ map drawChar (zipWithPad ' ' ' ' (word w) (input w)) ++ [withAttr norm (str " ")]
 
 drawChar :: (Char, Char) -> Widget n
@@ -111,48 +77,6 @@ zipWithPad :: a -> b -> [a] -> [b] -> [(a, b)]
 zipWithPad a b (x : xs) (y : ys) = (x, y) : zipWithPad a b xs ys
 zipWithPad a _ [] ys = zip (repeat a) ys
 zipWithPad _ b xs [] = zip xs (repeat b)
-
-cursorPosition :: NonEmptyCursor a -> Int
-cursorPosition cur = length (nonEmptyCursorPrev cur)
-
-handleTextInput :: TestState -> Char -> EventM n (Next TestState)
-handleTextInput s c =
-  case c of
-    ' ' -> do
-      let cur = text s
-      case nonEmptyCursorSelectNext cur of
-        Nothing -> continue s
-        Just cur' -> continue $ s {text = cur'}
-    _ -> do
-      let cur = text s
-      let curword = nonEmptyCursorCurrent cur
-      let updatedWord = TestWord {word = word curword, input = input curword ++ [c]}
-      let new_text = reverse (nonEmptyCursorPrev cur) ++ [updatedWord] ++ nonEmptyCursorNext cur
-      case NE.nonEmpty new_text of
-        Nothing -> continue s
-        Just ne -> do
-          case makeNonEmptyCursorWithSelection (cursorPosition cur) ne of
-            Nothing -> continue s
-            Just ne' -> continue $ s {text = ne'}
-
-handleBackSpaceInput :: TestState -> EventM n (Next TestState)
-handleBackSpaceInput s = do
-  let cur = text s
-  let curword = nonEmptyCursorCurrent cur
-  case input curword of
-    "" -> do
-      case nonEmptyCursorSelectPrev cur of
-        Nothing -> continue s
-        Just cur' -> continue $ s {text = cur'}
-    _ -> do
-      let updatedWord = TestWord {word = word curword, input = init (input curword)}
-      let new_text = reverse (nonEmptyCursorPrev cur) ++ [updatedWord] ++ nonEmptyCursorNext cur
-      case NE.nonEmpty new_text of
-        Nothing -> continue s
-        Just ne -> do
-          case makeNonEmptyCursorWithSelection (cursorPosition cur) ne of
-            Nothing -> continue s
-            Just ne' -> continue $ s {text = ne'}
 
 handleInputEvent :: TestState -> BrickEvent n e -> EventM n (Next TestState)
 handleInputEvent s i =
