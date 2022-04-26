fb1dbac9a6d51853c46881564e15b8599106485c
Simon Hostettler <simon.hostettler@protonmail.com>
2022-04-26 14:39:29 +0200

    changed default color

--- app/Config.hs
+++ app/Config.hs
@@ -35,7 +35,7 @@ readConfig = do
     conf <- readFile fp
 
     let lines = splitOn "\n" conf
-    let fC = hexToRgb (fst (head (readHex (getSettingByName lines "fgcolor" "ff79c6"))))
+    let fC = hexToRgb (fst (head (readHex (getSettingByName lines "fgcolor" "88d0c0"))))
     let cS = read (getSettingByName lines "cursorshape" "5")
     let nC = read (getSettingByName lines "numcommonwords" "250")
 
